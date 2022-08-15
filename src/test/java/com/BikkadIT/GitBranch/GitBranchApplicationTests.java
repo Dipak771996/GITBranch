be315cda565224e6b8125209df2f5e06fe1ddd16
Dipak771996 <deepakpidurkar77@gmail.com>
2022-08-15 10:20:21 +0530

    Project Structure Created

---
+++ src/test/java/com/BikkadIT/GitBranch/GitBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.GitBranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
